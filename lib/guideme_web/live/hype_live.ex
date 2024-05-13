daec031d4dc4bae6432c8e5945b53586e3951938
Kyle Beechly <contact@kabeech.com>
2024-05-12 22:24:22 -0700

    Set initial hype

--- lib/guideme_web/live/hype_live.ex
+++ lib/guideme_web/live/hype_live.ex
@@ -2,30 +2,31 @@ defmodule GuidemeWeb.HypeLive do
   use GuidemeWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, hype: 50)}
+    {:ok, assign(socket, hype: 10)}
   end
 
   def render(assigns) do
     ~H"""
-      <h1>Hype</h1>
-      <div >
-        <div style="width: 100%" class="meter">
-          <span style={"display: block; width: #{@hype}%; background-color: #00ddbb;"}><%= @hype %>%</span>
-        </div>
+    <h1>Hype</h1>
+    <div>
+      <div style="width: 100%" class="meter">
+        <span style={"display: block; width: #{@hype}%; background-color: #00ddbb;"}>
+          <%= @hype %>%
+        </span>
       </div>
-      <div >
-        <button phx-click="increase_brightness">Increase hype</button>
-        <button phx-click="decrease_brightness">Decrease hype</button>
-      </div>
+    </div>
+    <div>
+      <button phx-click="increase_hype">Increase hype</button>
+      <button phx-click="decrease_hype">Decrease hype</button>
+    </div>
     """
   end
- 
-  def handle_event("increase_brightness", _value, socket) do
+
+  def handle_event("increase_hype", _value, socket) do
     {:noreply, assign(socket, hype: socket.assigns.hype + 10)}
   end
 
-  def handle_event("decrease_brightness", _value, socket) do
+  def handle_event("decrease_hype", _value, socket) do
     {:noreply, assign(socket, hype: socket.assigns.hype - 10)}
   end
-
 end
