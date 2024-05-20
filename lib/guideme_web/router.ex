22a53ee5a438aed64c21cb6573377940c5218196
Kyle Beechly <contact@kabeech.com>
2024-05-20 00:09:44 -0700

    Prep for auth

--- lib/guideme_web/router.ex
+++ lib/guideme_web/router.ex
@@ -2,26 +2,26 @@ defmodule GuidemeWeb.Router do
   use GuidemeWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {GuidemeWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {GuidemeWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", GuidemeWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
 
-    live "/hype", HypeLive
-    live "/boilerplate-docs", BoilerplateDocsLive
-    live "/guide", GuideLive
+    live("/hype", HypeLive)
+    live("/boilerplate-docs", BoilerplateDocsLive)
+    live("/guide", GuideLive)
   end
 
   # Other scopes may use custom stacks.
@@ -39,10 +39,10 @@ defmodule GuidemeWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: GuidemeWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: GuidemeWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
