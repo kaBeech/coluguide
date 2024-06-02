bf2c8a43c7740e56c3fa0bce17c12df4983c3aa2
Kyle Beechly <contact@kabeech.com>
2024-06-02 15:15:32 -0700

    Update migration to add user role

--- priv/repo/migrations/20240520071844_create_users.exs
+++ priv/repo/migrations/20240520071844_create_users.exs
@@ -5,6 +5,7 @@ defmodule Guideme.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :email, :string, null: false
       add :password_hash, :string
+      add :role, :string, default: "user"
 
       timestamps()
     end
