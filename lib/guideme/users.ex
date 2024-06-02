5e6f629874840e1f1b8c4fde6f618902fff74da8
Kyle Beechly <contact@kabeech.com>
2024-06-02 14:33:31 -0700

    Add user roles

---
+++ lib/guideme/users.ex
@@ -0,0 +1,42 @@
+defmodule Guideme.Users do
+  alias Guideme.{Repo, Users.User}
+
+  @type t :: %User{}
+
+  @spec create_admin(map()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def create_admin(params) do
+    %User{}
+    |> User.changeset(params)
+    |> User.changeset_role(%{role: "admin"})
+    |> Repo.insert()
+  end
+
+  @spec set_admin_role(t()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def set_admin_role(user) do
+    user
+    |> User.changeset_role(%{role: "admin"})
+    |> Repo.update()
+  end
+
+  @spec create_editor(map()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def create_editor(params) do
+    %User{}
+    |> User.changeset(params)
+    |> User.changeset_role(%{role: "editor"})
+    |> Repo.insert()
+  end
+
+  @spec set_editor_role(t()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def set_editor_role(user) do
+    user
+    |> User.changeset_role(%{role: "editor"})
+    |> Repo.update()
+  end
+
+  @spec set_user_role(t()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def set_user_role(user) do
+    user
+    |> User.changeset_role(%{role: "user"})
+    |> Repo.update()
+  end
+end
---
+++ test/guideme/users/user_test.exs
@@ -0,0 +1,30 @@
+defmodule Guideme.Users.UserTest do
+  use Guideme.DataCase
+
+  alias Guideme.Users.User
+
+  test "changeset/2 sets default role" do
+    user =
+      %User{}
+      |> User.changeset(%{})
+      |> Ecto.Changeset.apply_changes()
+
+    assert user.role == "user"
+  end
+
+  test "changeset_role/2" do
+    changeset = User.changeset_role(%User{}, %{role: "invalid"})
+
+    assert changeset.errors[:role] ==
+             {"is invalid", [validation: :inclusion, enum: ["user", "editor", "admin"]]}
+
+    changeset = User.changeset_role(%User{}, %{role: "admin"})
+    refute changeset.errors[:role]
+
+    changeset = User.changeset_role(%User{}, %{role: "editor"})
+    refute changeset.errors[:role]
+
+    changeset = User.changeset_role(%User{}, %{role: "user"})
+    refute changeset.errors[:role]
+  end
+end
