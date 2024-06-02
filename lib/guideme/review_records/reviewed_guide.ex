2eef93fbd9ec8651c5097b252bb501daabc2e732
Kyle Beechly <contact@kabeech.com>
2024-06-02 16:25:56 -0700

    Add Review Records

---
+++ lib/guideme/review_records/reviewed_guide.ex
@@ -0,0 +1,19 @@
+defmodule Guideme.ReviewRecords.ReviewedGuide do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "reviewed_guides" do
+    field :reviewed_at, :date
+    belongs_to :user, Guideme.Users.User
+    belongs_to :guide, Guideme.Guides.Guide
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(reviewed_guide, attrs) do
+    reviewed_guide
+    |> cast(attrs, [:reviewed_at, :user_id, :guide_id])
+    |> validate_required([:reviewed_at, :user_id, :guide_id])
+  end
+end
