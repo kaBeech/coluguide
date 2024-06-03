cae3ceafe392c7a2b519dceeea1f377f5fcca2fb
Kyle Beechly <contact@kabeech.com>
2024-06-03 02:53:11 -0700

    Add boilerplate steps display

--- lib/guideme_web/live/guide_live.ex
+++ lib/guideme_web/live/guide_live.ex
@@ -1,153 +1,84 @@
 defmodule GuidemeWeb.GuideLive do
+  use GuidemeWeb, :live_view
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
     {:ok, assign(socket, guide: nil)}
   end
 
-  def render(assigns) do
+  def example_steps do
+    [
+      %{
+        full_text: "Step 1"
+      },
+      %{
+        full_text: "Step 2"
+      },
+      %{
+        full_text: "Step 3"
+      }
+    ]
+  end
+
+  def render_step(assigns) do
     ~H"""
     <div class="flex textBigger justifyCenter gap1">
-      <span class="link" onMouseEnter={} onMouseLeave={}>
+      <span id="link1" phx-click="toggle" phx-value-to="#link1" class="link">
         
       </span>
-      <span class="link" onMouseEnter={} onMouseLeave={}>
+      <span class="link">
         
       </span>
-      <span class="link" onMouseEnter={} onMouseLeave={}>
+      <span class="link">
         
       </span>
       <span>
-        <input type="checkbox" onMouseEnter={} onMouseLeave={} />
+        <input type="checkbox" />
+      </span>
+      <span class="marginBottom1">
+        <%= @full_text %>
+      </span>
+    </div>
+    """
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="flex textBigger justifyCenter gap1">
+      <span id="link1" phx-click="toggle" phx-value-to="#link1" class="link">
+        
+      </span>
+      <span class="link">
+        
+      </span>
+      <span class="link">
+        
+      </span>
+      <span>
+        <input type="checkbox" />
       </span>
     </div>
     <h2>
       Please select an option below by clicking an <span class="link">orange file </span>
     </h2>
+    <%= for step <- [
+      %{
+        full_text: "Step 1"
+      },
+      %{
+        full_text: "Step 2"
+      },
+      %{
+        full_text: "Step 3"
+      }
+    ] do %>
+      <%= render_step(step) %>
+    <% end %>
     """
   end
 
-  # def render(assigns) do
-  #   ~H"""
-  #   <div class="flex textBigger justifyCenter gap1">
-  #     <span class="link" onMouseEnter={showExternalLink} onMouseLeave={hideExternalLink}>
-  #       
-  #     </span>
-  #     <span class="link" onMouseEnter={showImage} onMouseLeave={hideImage}>
-  #       
-  #     </span>
-  #     <span class="link" onMouseEnter={showFile} onMouseLeave={hideFile}>
-  #       
-  #     </span>
-  #     <span>
-  #       <input type="checkbox" onMouseEnter={showCheckbox} onMouseLeave={hideCheckbox} />
-  #     </span>
-  #   </div>
-  #   <h2>
-  #     Please select an option below by clicking an <span class="link">orange file </span>
-  #   </h2>
-  #   """
-  # end
-  #
-  # def hideIconWrapper(hiderFunction) do
-  #   :timer.sleep(4000)
-  #   hiderFunction.("true")
-  # end
-  #
-  # def showIconWrapper(showerFunction) do
-  #   setExternalLinkHidden("true")
-  #   setImageHidden("true")
-  #   setFileHidden("true")
-  #   setCheckboxHidden("true")
-  #   :timer.sleep(4000)
-  #   showerFunction.("false")
-  # end
-  #
-  # def showExternalLink do
-  #   showIconWrapper(&setExternalLinkHidden/1)
-  # end
-  #
-  # def hideExternalLink do
-  #   hideIconWrapper(&setExternalLinkHidden/1)
-  # end
-  #
-  # def showImage do
-  #   showIconWrapper(&setImageHidden/1)
-  # end
-  #
-  # def hideImage do
-  #   hideIconWrapper(&setImageHidden/1)
-  # end
-  #
-  # def showFile do
-  #   showIconWrapper(&setFileHidden/1)
-  # end
-  #
-  # def hideFile do
-  #   hideIconWrapper(&setFileHidden/1)
-  # end
-  #
-  # def showCheckbox do
-  #   showIconWrapper(&setCheckboxHidden/1)
-  # end
-  #
-  # def hideCheckbox do
-  #   hideIconWrapper(&setCheckboxHidden/1)
-  # end
-  #
-  # def setExternalLinkHidden(hidden, socket) do
-  #   {:noreply, assign(socket, externalLinkHidden: hidden)}
-  # end
-  #
-  # def setImageHidden(hidden, socket) do
-  #   {:noreply, assign(socket, imageHidden: hidden)}
-  # end
-  #
-  # def setFileHidden(hidden, socket) do
-  #   {:noreply, assign(socket, fileHidden: hidden)}
-  # end
-  #
-  # def setCheckboxHidden(hidden, socket) do
-  #   {:noreply, assign(socket, checkboxHidden: hidden)}
-  # end
-  #
-  # def handle_event("showExternalLink", _value, socket) do
-  #   setExternalLinkHidden("false", socket)
-  # end
-  #
-  # def handle_event("hideExternalLink", _value, socket) do
-  #   setExternalLinkHidden("true", socket)
-  # end
-  #
-  # def handle_event("showImage", _value, socket) do
-  #   setImageHidden("false", socket)
-  # end
-  #
-  # def handle_event("hideImage", _value, socket) do
-  #   setImageHidden("true", socket)
-  # end
-  #
-  # def handle_event("showFile", _value, socket) do
-  #   setFileHidden("false", socket)
-  # end
-  #
-  # def handle_event("hideFile", _value, socket) do
-  #   setFileHidden("true", socket)
-  # end
-  #
-  # def handle_event("showCheckbox", _value, socket) do
-  #   setCheckboxHidden("false", socket)
-  # end
-  #
-  # def handle_event("hideCheckbox", _value, socket) do
-  #   setCheckboxHidden("true", socket)
-  # end
-  #
-  # def handle_info(:timeout, socket) do
-  #   setExternalLinkHidden("true", socket)
-  #   setImageHidden("true", socket)
-  #   setFileHidden("true", socket)
-  #   setCheckboxHidden("true", socket)
-  #   {:noreply, socket}
-  # end
+  def handle_event("toggle", %{"to" => to}, socket) do
+    JS.toggle(to: to)
+    {:noreply, socket}
+  end
 end
