cd850e39aa4650b4a14c4d5442d145aa8049e52d
kaBeech <contact@kaBeech.com>
2024-06-19 23:08:49 -0700

    Add dummy image toggling

--- lib/guideme_web/components/step.ex
+++ lib/guideme_web/components/step.ex
@@ -3,14 +3,21 @@ defmodule Step do
   use Phoenix.Component
   alias Phoenix.LiveView.JS
 
+  defp get_last_char(text) do
+    String.slice(text, -2..-1)
+  end
+
   def render_step(assigns) do
     ~H"""
     <div class="flex justifyLeft gap1">
       <span class="textDull">
         
       </span>
-      <span class="textDull">
+      <span class="textDull" phx-click={JS.toggle(to: "#image-step" <> @number)}>
         
+      </span>
+      <span id={"image-step" <> @number} class="textDull hidden">
+        IMAGE!
       </span>
       <span class="textDull">
         
