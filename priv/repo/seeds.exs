38aef84120d6b65a18a28129769cba3e9fed2706
kaBeech <contact@kaBeech.com>
2024-06-26 14:15:22 -0700

    Add test seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,43 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+Guideme.Repo.insert!(%Guideme.Guides.Template{
+  name: "chapter"
+})
+
+Guideme.Repo.insert!(%Guideme.Guides.Template{
+  name: "standard"
+})
+
+Guideme.Repo.insert!(%Guideme.Images.Image{
+  src: "icon.svg"
+})
+
 Guideme.Repo.insert!(%Guideme.Chapters.Chapter{
   title: "Test Chapter"
+})
+
+Guideme.Repo.insert!(%Guideme.Guides.Guide{
+  title: "Test Guide",
+  short_title: "Test Guide",
+  template_id: 1,
+  chapter_id: 1
+})
+
+Guideme.Repo.insert!(%Guideme.Steps.Step{
+  full_text: "Test Step 1",
+  guide_id: 1,
+  number: 1,
+  external_link: "https://hexdocs.pm/"
+})
+
+Guideme.Repo.insert!(%Guideme.Steps.StepImage{
+  image_id: 1,
+  alt: "Test Image for Step 1",
+  step_id: 1
+})
+
+Guideme.Repo.insert!(%Guideme.DetailsLinks.DetailsLink{
+  step_id: 1,
+  guide_id: 1
 })
