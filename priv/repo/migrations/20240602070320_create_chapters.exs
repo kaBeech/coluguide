b86485fe377afd71ac8dabdd728f20ad84788b97
kaBeech <contact@kaBeech.com>
2024-06-26 13:00:17 -0700

    Add unique index to Chapter

--- priv/repo/migrations/20240602070320_create_chapters.exs
+++ priv/repo/migrations/20240602070320_create_chapters.exs
@@ -7,5 +7,12 @@ defmodule Guideme.Repo.Migrations.CreateChapters do
 
       timestamps(type: :utc_datetime)
     end
+
+    create(
+      unique_index(
+        :chapters,
+        [:title]
+      )
+    )
   end
 end
