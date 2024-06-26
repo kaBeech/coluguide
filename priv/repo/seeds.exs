a6806c1fe1f44d15df0ebc4120e21045b038ef50
kaBeech <contact@kaBeech.com>
2024-06-26 13:00:51 -0700

    Seed a test chapter

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Guideme.Repo.insert!(%Guideme.Chapters.Chapter{
+  title: "Test Chapter"
+})
