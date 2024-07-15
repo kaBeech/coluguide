33aef40fa47f8e9ceeb7874899bf851cf1ba4c7d
kaBeech <contact@kaBeech.com>
2024-07-14 19:39:18 -0700

    Only show tutorial hints if template is Chapter

--- lib/guideme_web/live/guide_live.ex
+++ lib/guideme_web/live/guide_live.ex
@@ -53,23 +53,25 @@ defmodule GuidemeWeb.GuideLive do
   def render(assigns) do
     ~H"""
     <div class="flex column alignCenter textBigger">
-      <div class="flex justifyCenter gap1">
-        <span class="link">
-          
-        </span>
-        <span class="link">
-          
-        </span>
-        <span class="link">
-          
-        </span>
-        <span>
-          <input type="checkbox" />
-        </span>
-      </div>
-      <h2>
-        Please select an option below by clicking an <span class="link">orange file </span>
-      </h2>
+      <%= if @guide.template == "chapter" do %>
+        <div class="flex justifyCenter gap1">
+          <span class="link">
+            
+          </span>
+          <span class="link">
+            
+          </span>
+          <span class="link">
+            
+          </span>
+          <span>
+            <input type="checkbox" />
+          </span>
+        </div>
+        <h2>
+          Please select an option below by clicking an <span class="link">orange file </span>
+        </h2>
+      <% end %>
       <h1>
         <%= @guide.title %>
       </h1>
