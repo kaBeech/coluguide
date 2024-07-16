d78e83cd55b4e713ee8258726ad35d400e23a9db
kaBeech <contact@kaBeech.com>
2024-07-16 14:20:12 -0700

    Get chapter from params in New Guide

--- lib/guideme_web/live/guide_live/new.ex
+++ lib/guideme_web/live/guide_live/new.ex
@@ -2,7 +2,7 @@ defmodule GuidemeWeb.GuideLive.New do
   use GuidemeWeb, :live_view
   use Phoenix.LiveView
 
-  alias Guideme.{Guides, Guides.Guide}
+  alias Guideme.{Guides, Guides.Guide, Chapters}
 
   @impl true
   def mount(_params, _session, socket) do
@@ -16,10 +16,17 @@ defmodule GuidemeWeb.GuideLive.New do
   end
 
   @impl true
+  def handle_params(%{"id" => id}, _, socket) do
+    {:noreply,
+     socket
+     |> assign(:chapter, Chapters.get_chapter!(id))}
+  end
+
+  @impl true
   def handle_event("create", %{"guide" => params}, socket) do
     case Guides.create_guide(params) do
       {:ok, _guide} ->
-        changeset = Guides.change_guide(%Guide{})
+        changeset = Guides.change_guide(socket.assigns.chapter, %Guide{})
         {:noreply, assign(socket, form: to_form(changeset))}
 
       {:error, %Ecto.Changeset{} = changeset} ->
