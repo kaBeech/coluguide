ca42822e6684c8f3c66de95c8232a2069a25bfaa
kaBeech <contact@kaBeech.com>
2024-09-24 22:51:56 -0700

    Document schema

--- lib/guideme/review_records/reviewed_guide.ex
+++ lib/guideme/review_records/reviewed_guide.ex
@@ -1,11 +1,23 @@
 defmodule Guideme.ReviewRecords.ReviewedGuide do
+  @moduledoc """
+  Each row represents a review of a Guide by a specific User.
+
+  An admin User can assign Users to review specific Guides. When a Guide is
+  updated with changes needing review, Users assigned to review it will be
+  notified. This way, an administrator can have feedback that their Users are
+  keeping up to date with the current information.
+  """
   use Ecto.Schema
   import Ecto.Changeset
 
   schema "reviewed_guides" do
+    # The date and time the Guide was last reviewed
     field :reviewed_at, :utc_datetime
+    # The User reviewing the guide
     belongs_to :user, Guideme.Users.User
+    # The Guide being reviewed
     belongs_to :guide, Guideme.Guides.Guide
+    # The admin User who assigned the review
     has_one :review_assigned_by, Guideme.Users.User
 
     timestamps(type: :utc_datetime)
