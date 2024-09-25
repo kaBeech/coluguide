215f1c8513b8acaf26d178451c880ae493b80619
kaBeech <contact@kaBeech.com>
2024-09-24 20:45:07 -0700

    Add schema documentation

--- lib/guideme/guides/template.ex
+++ lib/guideme/guides/template.ex
@@ -1,4 +1,17 @@
 defmodule Guideme.Guides.Template do
+  @moduledoc """
+  Names of Templates used to display Guides.
+
+  Guides will be presented according to a Template. The two default Templates
+  are "chapter", used for the index page of a chapter, which adds tutorial
+  information at the top of the page, and "standard", which is used for most
+  Guides and omits the tutorial information
+
+
+  Custom Templates may include space for additional information or custom
+  modules, such as a chess clock for a timekeeping Guide within a chess
+  Chapter.
+  """
   use Ecto.Schema
   import Ecto.Changeset
 
