74aca9efb210a64a1670b60aefdb0783e51397b9
kaBeech <contact@kaBeech.com>
2024-09-27 15:35:45 -0700

    Scaffold MyReviews view

---
+++ lib/guideme_web/live/reviewed_guide_live/mine.ex
@@ -0,0 +1,54 @@
+defmodule GuidemeWeb.ReviewedGuideLive.Mine do
+  use GuidemeWeb, :live_view
+
+  alias Guideme.ReviewRecords
+  alias Guideme.Guides
+  alias GuidemeWeb.Guide.ReviewStatus
+
+  @impl true
+  def mount(_params, _session, socket) do
+    my_reviewed_guides = ReviewRecords.list_my_reviewed_guides(socket.assigns.current_user.id)
+    my_guides = Guides.list_guides_for_my_reviews()
+
+    {needs_review, stale, reviewed, unreviewed} =
+      ReviewStatus.sort_guides_by_review_status(my_reviewed_guides, my_guides, [], [], [])
+
+    {:ok,
+     assign(socket,
+       needs_review: %{
+         name: "Needs Review!",
+         guides: needs_review,
+         action: "select_needs_review"
+       },
+       stale: %{name: "Stale Reviews", guides: stale, action: "select_stale"},
+       reviewed: %{name: "Reviewed", guides: reviewed, action: "select_reviewed"},
+       unreviewed: %{name: "Unreviewed", guides: unreviewed, action: "select_unreviewed"},
+       current_selection: nil
+     )}
+  end
+
+  @impl true
+  def handle_info(
+        {GuidemeWeb.ReviewedGuideLive.FormComponent, {:saved, reviewed_guide}},
+        socket
+      ) do
+    {:noreply, stream_insert(socket, :reviewed_guides, reviewed_guide)}
+  end
+
+  @impl true
+  def handle_event("select_needs_review", _, socket) do
+    {:noreply, assign(socket, current_selection: "needs_review")}
+  end
+
+  def handle_event("select_stale", _, socket) do
+    {:noreply, assign(socket, current_selection: "stale")}
+  end
+
+  def handle_event("select_reviewed", _, socket) do
+    {:noreply, assign(socket, current_selection: "reviewed")}
+  end
+
+  def handle_event("select_unreviewed", _, socket) do
+    {:noreply, assign(socket, current_selection: "unreviewed")}
+  end
+end
