5a0da1b7159c4ce98e5b7702eb198c8c65522ea4
kaBeech <contact@kaBeech.com>
2024-09-27 19:24:06 -0700

    Improve table formatting

--- lib/guideme_web/components/core_components.ex
+++ lib/guideme_web/components/core_components.ex
@@ -475,8 +475,8 @@ defmodule GuidemeWeb.CoreComponents do
       end
 
     ~H"""
-    <div class="overflow-y-auto px-4 sm:overflow-visible sm:px-0">
-      <table class="w-[40rem] mt-11 sm:w-full">
+    <div class="flex column alignCenter overflow-y-auto px-4 sm:overflow-visible sm:px-0">
+      <table class="margin1 w-[40rem] mt-11 sm:w-full">
         <thead class="text-sm text-left leading-6 text-zinc-500">
           <tr>
             <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal"><%= col[:label] %></th>
