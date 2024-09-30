1138cfc98b83d13debb0ef213ac8335239ea054a
kaBeech <contact@kaBeech.com>
2024-09-29 19:16:57 -0700

    Add smarter searching with Step text

--- lib/guideme_web/components/search/search.ex
+++ lib/guideme_web/components/search/search.ex
@@ -19,11 +19,27 @@ defmodule GuideMeWeb.Search do
   end
 
   def search_guides(query, socket) do
-    search_results =
-      Enum.filter(GuideMe.Guides.list_guides(), fn guide ->
+    guide_names_containing_query =
+      Enum.filter(socket.assigns.guide_names, fn guide ->
         String.contains?(String.downcase(guide.name), String.downcase(query))
       end)
 
+    steps_containing_query =
+      Enum.filter(socket.assigns.step_texts, fn step ->
+        String.contains?(String.downcase(step.full_text), String.downcase(query))
+      end)
+
+    guides_with_query_in_steps_but_not_name =
+      Enum.filter(socket.assigns.guide_names, fn guide ->
+        Enum.any?(steps_containing_query, fn step ->
+          step.guide_id == guide.id &&
+            !Enum.any?(guide_names_containing_query, fn guide_name ->
+              guide_name.id == guide.id
+            end)
+        end)
+      end)
+
+    search_results = guide_names_containing_query ++ guides_with_query_in_steps_but_not_name
     {:noreply, assign(socket, :search_results, search_results)}
   end
 end
