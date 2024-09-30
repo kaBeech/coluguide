ba876beed343a4345039bff6e5d538babbd05e2b
kaBeech <contact@kaBeech.com>
2024-09-29 23:34:40 -0700

    Make Back button snappier
    For some reason .link navigate was causing slowdowns. My guess is
    the extra JS in the one was getting in the way somehow

--- lib/guideme_web/components/navbar/navbar.ex
+++ lib/guideme_web/components/navbar/navbar.ex
@@ -5,7 +5,7 @@ defmodule NavBar do
   def render_navbar(assigns) do
     ~H"""
     <nav class="flex justifyCenter gap1 widthFull marginTop2 marginRight1Point5">
-      <.link navigate="javascript:window.history.back();"><%= "<-- Back" %></.link>
+      <a href="javascript:window.history.back();"><%= "<-- Back" %></a>
       <span class="textDull">|</span>
       <.link navigate="/">Home</.link>
     </nav>
