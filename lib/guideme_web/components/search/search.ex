8e4fe0f2a960b2c678121ed2295e5b2513e4e17b
kaBeech <contact@kaBeech.com>
2024-09-29 19:23:34 -0700

    Search based on individual words not strings

--- lib/guideme_web/components/search/search.ex
+++ lib/guideme_web/components/search/search.ex
@@ -19,27 +19,35 @@ defmodule GuideMeWeb.Search do
   end
 
   def search_guides(query, socket) do
-    guide_names_containing_query =
+    query_words = String.split(query, " ")
+
+    guide_names_with_all_query_words =
       Enum.filter(socket.assigns.guide_names, fn guide ->
-        String.contains?(String.downcase(guide.name), String.downcase(query))
+        Enum.all?(query_words, fn query_word ->
+          String.contains?(String.downcase(guide.name), String.downcase(query_word))
+        end)
       end)
 
-    steps_containing_query =
+    steps_with_all_query_words =
       Enum.filter(socket.assigns.step_texts, fn step ->
-        String.contains?(String.downcase(step.full_text), String.downcase(query))
+        Enum.all?(query_words, fn query_word ->
+          String.contains?(String.downcase(step.full_text), String.downcase(query_word))
+        end)
       end)
 
     guides_with_query_in_steps_but_not_name =
       Enum.filter(socket.assigns.guide_names, fn guide ->
-        Enum.any?(steps_containing_query, fn step ->
+        Enum.any?(steps_with_all_query_words, fn step ->
           step.guide_id == guide.id &&
-            !Enum.any?(guide_names_containing_query, fn guide_name ->
+            !Enum.any?(guide_names_with_all_query_words, fn guide_name ->
               guide_name.id == guide.id
             end)
         end)
       end)
 
-    search_results = guide_names_containing_query ++ guides_with_query_in_steps_but_not_name
+    search_results =
+      guide_names_with_all_query_words ++ guides_with_query_in_steps_but_not_name
+
     {:noreply, assign(socket, :search_results, search_results)}
   end
 end
