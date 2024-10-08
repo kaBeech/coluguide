d3504d075453f484b98eb70a2894bf8878e7eebd
kaBeech <contact@kaBeech.com>
2024-10-08 16:09:09 -0700

    Fix CRUD display

--- lib/coluguide_web/components/core_components.ex
+++ lib/coluguide_web/components/core_components.ex
@@ -50,9 +50,9 @@ defmodule ColuguideWeb.CoreComponents do
       data-cancel={JS.exec(@on_cancel, "phx-remove")}
       class="relative z-50 hidden"
     >
-      <div id={"#{@id}-bg"} class="bg-zinc-50/90 fixed inset-0 transition-opacity" aria-hidden="true" />
+      <div id={"#{@id}-bg"} class="bg-zinc-50/90 inset-0 transition-opacity" aria-hidden="true" />
       <div
-        class="fixed inset-0 overflow-y-auto"
+        class="inset-0 overflow-y-auto"
         aria-labelledby={"#{@id}-title"}
         aria-describedby={"#{@id}-description"}
         role="dialog"
